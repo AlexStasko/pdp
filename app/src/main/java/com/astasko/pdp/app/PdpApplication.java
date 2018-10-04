1672b22dcf9119368c8f12662129ddd7a6dcd137
Aleksandr Stasko <aleksandr_stasko@epam.com>
2018-10-04 15:52:42 +0300

    Initialized Spring Boot

---
+++ app/src/main/java/com/astasko/pdp/app/PdpApplication.java
@@ -0,0 +1,12 @@
+package com.astasko.pdp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpApplication.class, args);
+	}
+}
